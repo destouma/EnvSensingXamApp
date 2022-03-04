812f0247b16995d372cfd444b2a65c81723b9222
destouma <mdsoftengr@gmail.com>
2022-03-04 16:03:22 -0500

    minor update

--- EnvSensingXamApp/Constants.cs
+++ EnvSensingXamApp/Constants.cs
@@ -3,7 +3,7 @@ namespace EnvSensingXamApp
 {
     public static class Constants
     {
-        public static string RestServer = "http://192.168.1.238";
+        public static string RestServer = "http://192.168.1.183";
 
     }
 }
